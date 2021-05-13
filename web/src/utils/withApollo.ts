be575bd70dcc062f270f0e72ce8aa93e15860de9
Robin Rawat <robinrawat7017@gmail.com>
2021-05-13 17:08:53 +0530

    ssr with apollo

---
+++ web/src/utils/withApollo.ts
@@ -0,0 +1,30 @@
+import { withApollo as createWithApollo } from "next-apollo";
+import { PaginatedPosts } from "../generated/graphql";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: process.env.NEXT_PUBLIC_API_URL as string,
+  credentials: "include",
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          posts: {
+            keyArgs: [],
+            merge(
+              existing: PaginatedPosts | undefined,
+              incoming: PaginatedPosts
+            ): PaginatedPosts {
+              return {
+                ...incoming,
+                posts: [...(existing?.posts || []), ...incoming.posts],
+              };
+            },
+          },
+        },
+      },
+    },
+  }),
+});
+
+export const withApollo = createWithApollo(client);
